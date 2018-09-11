f91a760033b9cb0060c4593bb14c1f348bf406e4
Justin Ahmann <jahmanndev@gmail.com>
2018-09-11 16:12:25 -0400

    data classes

---
+++ Flashcard.Data/Flashcard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcard.Data
+{
+    public class Flashcard
+    {
+
+        [Key]
+        public int CardID { get; set; }
+        public Guid UserID { get; set; }
+        public string Term { get; set; }
+        public string Definition { get; set; }
+        public int DeckIndex { get; set; }
+
+
+        public Flashcard()
+        {
+        }
+
+        public Flashcard(string term, string def, int deckindex)
+        {
+
+        }
+    }
+}
---
+++ Flashcard.Data/FlashcardEditDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcard.Data
+{
+    public class FlashcardEditDetails
+    {
+        [Key]
+        public int CardPropertieID { get; set; }
+        public int CardID { get; set; }
+        public Guid UserID { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime? ModifyTime { get; set; }
+        public DateTime LastReviewed { get; set; }
+        public int NumberTimesReviewed { get; set; }
+        public virtual Flashcard Flashcard { get; set; }
+    }
+}
