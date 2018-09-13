577592c06c2009604362236afe923c9b51c9edf3
Justin Ahmann <jahmanndev@gmail.com>
2018-09-13 09:57:52 -0400

    trash

---
+++ Flashcard.Model/FlashcardDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcard.Model
+{
+    class FlashcardDetails
+    {
+        public int CardID { get; set; }
+        public Guid UserID { get; set; }
+        public string Term { get; set; }
+        public string Definition { get; set; }
+        public int DeckIndex { get; set; }
+    }
+}
